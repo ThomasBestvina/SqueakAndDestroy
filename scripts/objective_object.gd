9cda084b3a58d517f2c3780c1f46b2d32555fde4
Thomas Bestvina <mail@thomasbestvina.dev>
2026-06-20 00:18:49 -0600

    cleans code, fixes scoring objects not reporting

--- scripts/objective_object.gd
+++ scripts/objective_object.gd
@@ -5,7 +5,7 @@ var already_scored = false
 
 
 @export var points_given: int = 50
-@export var min_velocity: float = 2.0
+@export var min_velocity: float = 5.0
 var initial_rotation: Vector3
 
 signal points_scored(amount: int)
@@ -13,14 +13,16 @@ signal points_scored(amount: int)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	contact_monitor = true
-	continuous_cd = true
+	max_contacts_reported = 10
 	StoatStash.safe_signal_connect(body_entered, body_collision)
 	initial_rotation = rotation
 
 func body_collision(body: Node):
 	if(body is RigidBody3D and not already_scored):
 		var impact_speed = body.linear_velocity.length()
+		print(impact_speed)
 		if impact_speed >= min_velocity:
+			already_scored = true
 			points_scored.emit(points_given)
 
 func _physics_process(_delta: float) -> void:
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,10 +1,12 @@
 extends Node3D
 
+var score: int = 0
 
 func _ready() -> void:
 	for i in $ScoringObjects.get_children():
 		if(i is ObjectiveObject):
-			StoatStash.safe_signal_connect(i.points_scored, score)
+			StoatStash.safe_signal_connect(i.points_scored, add_points)
 
-func score(points: int):
-	print(points)
+func add_points(points: int):
+	score += points
+	print("score!")
