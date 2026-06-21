c4b0fe332654205f60ac2f61b369701f6094a362
Thomas Bestvina <mail@thomasbestvina.dev>
2026-06-21 11:11:24 -0600

    hi

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -7,7 +7,8 @@ func _process(delta: float) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	visible = get_tree().paused
+	$Pause.visible = get_tree().paused
+	$Score.text = "Score: " + str(get_parent().score)
 
 func _on_resume_pressed() -> void:
 	get_tree().paused = false
